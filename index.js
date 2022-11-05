865ce846cf0765816b71bbff33c3e85341b9d9ac
maxkopysow <maxkopysow@gmail.com>
2022-11-05 14:48:42 +0300

    test

--- index.js
+++ index.js
@@ -6,65 +6,50 @@ const token = '5734831079:AAFB480eoh_PZzlygAITeo9IIWBD5wKCGIs';
 const webAppUrl= 'https://jocular-tulumba-eef92b.netlify.app/';
 const bot = new TelegramBot(token, {polling: true});
 const app = express();
+var chatID;
 app.use(express.json());
 app.use(cors());
 
 bot.on('message',async (msg) => {
     const chatId = msg.chat.id;
-    chatID = chatId;
     const text = msg.text;
+    
     if(text ==='/start'){
         var options = {
             reply_markup: ({
                 inline_keyboard: [
                     [{text: 'Заполните форму', web_app:{url: webAppUrl}}], // Clicking will send "1"
               ]
-        //       keyboard: [
-        //         [{text: 'Заполните форму', web_app:{url: webAppUrl}}], // Clicking will send "1"
-        //   ]
             })
         };
         await bot.sendMessage(chatId,'Ниже появится кнопка, заполните форму',options)
-
+  
     }
     if(msg?.web_app_data?.data){
         try{
             const data = JSON.parse(msg?.web_app_data?.data);
             console.log(data);
-            bot.sendMessage(chatId, 'FIO: ' + data?.FIO);
             bot.sendMessage(chatId,'Завяка отправлена на одобрение. Ожидайте ответа.');
 
             setTimeout( async ()=>{
 
             }, 3000)
-
         }catch(e){
             console.log(e);
-        }
-        
-    }
-  
+        }   
+    }  
 });
-app.get('', async (req,res) =>{
-    bot.sendMessage(chatID, 'FIO: ');
-    
-    return res.status(200).json(
-        "Hello"
-    );
-})
+app.get('/',async (req,res) =>{
+    const {queryId} = req.body;
+    console.log("Getting GET req");
+    console.log(req.body);
+    return res.status(200).json({});
 
+}) 
 app.post('/web-data',async (req,res) =>{
-   const {
-        queryId,
-        chatId,
-        usID
-    } = req.body;
-
+    const {queryId} = req.body;
     console.log("Getting post req");
-    console.log("qID " +queryId);
-    console.log("cID "+ chatId);
-    console.log(usID);
-    // bot.sendMessage(chatID, 'NO: ');
+    console.log(req.body);   // console.log("cID "+ chatId);
     // try{
     //     await bot.answerWebAppQuery(queryId,{
     //         type:'article',
@@ -74,6 +59,7 @@ app.post('/web-data',async (req,res) =>{
     //     })
     //     return res.status(200).json({});
     // }catch(e){
+    //     console.log(e);
     //     await bot.answerWebAppQuery(queryId,{
     //         type:'article',
     //         id:queryId,
@@ -82,12 +68,11 @@ app.post('/web-data',async (req,res) =>{
     //     })
     //     return res.status(500).json({});
     // }
-
+        return res.status(200).json({});
     
-    
-    return res.status(200).json({}); 
-
 })
+
+
 
 const PORT = 3000;
 
