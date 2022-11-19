cba9535f7eca1162e83fa4865ffdcdbef5516fdf
maxkopysow <maxkopysow@gmail.com>
2022-11-19 23:31:40 +0300

    added method to Elma

--- index.js
+++ index.js
@@ -40,38 +40,107 @@ bot.on('message',async (msg) => {
     }  
 });
 app.get('/',async (req,res) =>{
-    const {queryId} = req.body;
+    const {queryId, url} = req.body;
     console.log("Getting GET req");
     console.log(req.body);
+
+    await bot.answerWebAppQuery(queryId,{
+                type:'article',
+                id:queryId,
+
+                title:'Успешно отправлена заявка',
+                input_message_content:{message_text: url}
+            })
+
     return res.status(200).json({});
 
 }) 
+
 app.post('/web-data',async (req,res) =>{
     const {queryId} = req.body;
     console.log("Getting post req");
     console.log(req.body);   // console.log("cID "+ chatId);
-    // try{
-    //     await bot.answerWebAppQuery(queryId,{
-    //         type:'article',
-    //         id:queryId,
-    //         title:'Успешно отправлена заявка',
-    //         input_message_content:{message_text: "message text test"}
-    //     })
-    //     return res.status(200).json({});
-    // }catch(e){
-    //     console.log(e);
-    //     await bot.answerWebAppQuery(queryId,{
-    //         type:'article',
-    //         id:queryId,
-    //         title:'Не удалось отправить заявку',
-    //         input_message_content:{message_text: "Не удалось отправить заявку message text test"}
-    //     })
-    //     return res.status(500).json({});
-    // }
+ 
+ 
+    try{
+        sendDataToElma(req.body);
+        await bot.answerWebAppQuery(queryId,{
+            type:'article',
+            id:queryId,
+            title:'Успешно отправлена заявка',
+            input_message_content:{message_text: "message text test"}
+        })
         return res.status(200).json({});
+    }catch(e){
+        console.log(e);
+        await bot.answerWebAppQuery(queryId,{
+            type:'article',
+            id:queryId,
+            title:'Не удалось отправить заявку',
+            input_message_content:{message_text: "Не удалось отправить заявку message text test"}
+        })
+        return res.status(500).json({});
+    }
     
 })
 
+function sendDataToElma(reqBody){
+   const dataFromUser = {
+    queryId,
+    userName,
+    firstname,
+    middlename,
+    lastname,
+    email,
+    companyName,
+    companyINN,
+    phoneNumber,
+   } = reqBody;
+
+   const dataToElma = {
+        "context": {
+          "queryId":queryId,
+
+          "responsible_for_working_with_partners": [
+            "00000000-0000-0000-0000-000000000000"
+          ],
+          "email": [
+            {
+              "type": "main",
+              "email": email
+            }
+          ],
+          "full_name": {
+            "lastname": lastname,
+            "middlename": middlename,
+            "firstname": firstname
+          },
+          "company_name": companyName,
+          "company_inn": companyINN,
+          "phone_number": [
+            {
+              "type": "main",
+              "tel": phoneNumber
+            }
+          ],
+          "teg_telegram": userName,
+          "vneshnii_polzovatel": [
+            "00000000-0000-0000-0000-000000000000"
+          ],
+          "e_mail": email,
+          "__target": "example"
+        }
+   }
+
+
+
+   fetch('https://yhardd2fecpxq.elma365.ru/pub/v1/bpm/template/partner_account/processing_an_application_for_partner_registration/run', {  // Enter your IP address here
+            headers:{"Content-Type":"application/json"},
+            method: 'POST', 
+            body: JSON.stringify(dataToElma) // body data type must match "Content-Type" header
+            }
+        ) 
+}
 
 
 const PORT = 3000;
