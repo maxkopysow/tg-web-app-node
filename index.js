e8ef531fbfe35cffd40246eaa37ad13c48991367
maxkopysow <maxkopysow@gmail.com>
2022-11-20 20:38:14 +0300

    prealpha

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const cors = require('cors');
-
+const urlElma = 'https://yhardd2fecpxq.elma365.ru/pub/v1/bpm/template/partner_account/processing_an_application_for_partner_registration/run';
+const tokenElma = 'd49e5518-c455-4a14-aaa7-a0fc67f50ca1';
 const token = '5734831079:AAFB480eoh_PZzlygAITeo9IIWBD5wKCGIs';
 const webAppUrl= 'https://jocular-tulumba-eef92b.netlify.app/';
 const bot = new TelegramBot(token, {polling: true});
 const app = express();
-var chatID;
+
+
 app.use(express.json());
 app.use(cors());
 
@@ -56,28 +58,63 @@ app.get('/',async (req,res) =>{
 
 }) 
 
+app.post('/accepted', async (req, res)=>{
+    const {queryid, inviteUrl} = req.body;
+
+    const message = "Заявка была одобрена." +
+    + " Перейдите по ссылке для завершения регистрации: " + inviteUrl; 
+
+    try{
+        await bot.answerWebAppQuery(queryId,{
+            type:'article',
+            id:queryid,
+            title:'Заявка одобрена',
+            input_message_content:{message_text: message}
+        })
+        return res.status(200).json({});
+    }catch(e){
+        return res.status(500).json({});
+    }
+})
+
+app.post('/rejected', async (req, res)=>{
+    const {queryid, msg} = req.body;
+
+    const message = "Отказано в заявке :" + msg; 
+
+    try{
+        await bot.answerWebAppQuery(queryId,{
+            type:'article',
+            id:queryid,
+            title:'Отказ в заявке',
+            input_message_content:{message_text: message}
+        })
+        return res.status(200).json({});
+    }catch(e){
+        return res.status(500).json({});
+    }
+})
+
 app.post('/web-data',async (req,res) =>{
     const {queryId} = req.body;
     console.log("Getting post req");
-    console.log(req.body);   // console.log("cID "+ chatId);
- 
- 
+    console.log(req.body);   
+    const message = "Заявка отправлена. Ожидайте решения." ;
     try{
         sendDataToElma(req.body);
         await bot.answerWebAppQuery(queryId,{
             type:'article',
             id:queryId,
             title:'Успешно отправлена заявка',
-            input_message_content:{message_text: "message text test"}
+            input_message_content:{message_text: message}
         })
         return res.status(200).json({});
     }catch(e){
-        console.log(e);
         await bot.answerWebAppQuery(queryId,{
             type:'article',
             id:queryId,
             title:'Не удалось отправить заявку',
-            input_message_content:{message_text: "Не удалось отправить заявку message text test"}
+            input_message_content:{message_text: "Не удалось отправить заявку."}
         })
         return res.status(500).json({});
     }
@@ -85,7 +122,7 @@ app.post('/web-data',async (req,res) =>{
 })
 
 function sendDataToElma(reqBody){
-   const dataFromUser = {
+    const dataFromUser = {
     queryId,
     userName,
     firstname,
@@ -99,8 +136,6 @@ function sendDataToElma(reqBody){
 
    const dataToElma = {
         "context": {
-          "queryId":queryId,
-
           "responsible_for_working_with_partners": [
             "00000000-0000-0000-0000-000000000000"
           ],
@@ -128,14 +163,12 @@ function sendDataToElma(reqBody){
             "00000000-0000-0000-0000-000000000000"
           ],
           "e_mail": email,
+          "queryid":queryId,
           "__target": "example"
         }
    }
-
-
-
-   fetch('https://yhardd2fecpxq.elma365.ru/pub/v1/bpm/template/partner_account/processing_an_application_for_partner_registration/run', {  // Enter your IP address here
-            headers:{"Content-Type":"application/json"},
+   fetch(urlElma, {  // Enter your IP address here
+            headers:{"Content-Type":"application/json","Authorization": `Bearer ${tokenElma}`},
             method: 'POST', 
             body: JSON.stringify(dataToElma) // body data type must match "Content-Type" header
             }
